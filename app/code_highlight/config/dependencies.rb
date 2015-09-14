9bcbb69120e74daf7aa69de4d291494248720006
Ryan Stout <ryanstout@gmail.com>
2015-09-14 12:29:43 -0600

    fix for 0.9.5

--- app/code_highlight/config/dependencies.rb
+++ app/code_highlight/config/dependencies.rb
@@ -1,5 +1,5 @@
 # Component dependencies
 disable_auto_import
 
-css_file '/assets/code_highlight/assets/css/default.css'
-javascript_file '/assets/code_highlight/assets/js/highlight.pack.js'
+css_file '/app/code_highlight/assets/css/default.css'
+javascript_file '/app/code_highlight/assets/js/highlight.pack.js'
